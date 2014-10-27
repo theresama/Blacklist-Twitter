4662a073fd10bfa71c6efd1167b37303066ae30d
Theresa Ma <theresa.ma@mail.utoronto.ca>
2014-10-27 00:56:34 -0400

    First commit

---
+++ blacklist.js
@@ -0,0 +1,11 @@
+setInterval(
+	function(){
+		$('*:contains("Taylor Swift")').parent('.stream-item').remove();
+		$('*:contains("1989")').parent('.stream-item').remove();
+		$('*:contains("@taylorswift13")').parent('.stream-item').remove();
+		$('*:contains("taylor swift")').parent('.stream-item').remove();
+	}
+	, 3000);
+
+
+
