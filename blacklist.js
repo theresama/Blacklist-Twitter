42bb36d081b2ebf0f678ceb1deb8bd57fb67582d
Theresa Ma <theresa.ma@mail.utoronto.ca>
2014-10-27 13:25:58 -0400

    read in blacklist from txt file

--- blacklist.js
+++ blacklist.js
@@ -1,15 +1,24 @@
-
 var blacklist = new Array();
-blacklist[0] = "Taylor Swift";
-blacklist[1] = "1989";
-blacklist[2] = "@taylorswift13";
-blacklist[3] = "taylor swift";
 
 function addBlacklist(item){
 	blacklist.push(item);
 	console.log(item);
 }
 
+var xhr = new XMLHttpRequest();
+xhr.open('GET', chrome.extension.getURL('blacklist.txt'), true);
+
+xhr.onreadystatechange = function()
+{
+    if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200){
+    	var lines = xhr.responseText.split('\n');
+        for(var line = 0; line < lines.length; line++){
+          console.log(lines[line]);
+          blacklist.push(lines[line]);
+        }
+    }
+};
+xhr.send();
 
 
 function blockItems(){
@@ -17,10 +26,12 @@ function blockItems(){
 		function(){
 			for (var i = 0; i < blacklist.length; i++) {
 			    $('*:contains(' +  blacklist[i]  + ')').parent('.stream-item').remove();
+			    $('*:contains(' +  blacklist[i]  + ')').parent('.js-stream-item').remove();
 			}
 		}
 		, 1000);
 }
 
 blockItems();
+
 
